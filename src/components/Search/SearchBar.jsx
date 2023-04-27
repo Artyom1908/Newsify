004176c18dc97bacafbecceaf970a54e59284256
Artyom Artenyan <artyomartenyan.ait@gmail.com>
2023-04-27 21:07:30 +0300

    finish SearchBar

---
+++ src/components/Search/SearchBar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+
+function SearchBar({ onSearch }) {
+    const [query, setQuery] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSearch(query);
+        if (query === '') return
+        navigate(`/search/${query}`);
+        setQuery('')
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="relative flex items-center">
+                <input
+                    type="text"
+                    placeholder="Search articles"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    className="bg-gray-200 rounded-full py-2 px-4 w-full sm:w-80 text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-blue-400"
+                />
+                <button
+                    type="submit"
+                    className="absolute right-0 top-0 mt-2 mr-2 text-blue-400 hover:text-blue-500"
+                >
+                    <FontAwesomeIcon icon={faSearch} size="lg" />
+                </button>
+            </div>
+        </form>
+    );
+}
+
+export default SearchBar;
