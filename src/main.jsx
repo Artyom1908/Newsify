021acb46a4d79b3a2fc9863e7d19068c0c0b2cb3
Artyom Artenyan <artyomartenyan.ait@gmail.com>
2023-04-27 20:43:03 +0300

    create ArticleList

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './Css/tailwind.css';
+import { BrowserRouter } from 'react-router-dom';
+import store from "../src/Store/store";
+import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
