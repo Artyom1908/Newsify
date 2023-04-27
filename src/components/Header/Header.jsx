635c5d1d841cdcaba2ceaa479a9ef26898a963a5
Artyom Artenyan <artyomartenyan.ait@gmail.com>
2023-04-27 20:57:29 +0300

    Create and finished  Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import SearchBar from './SearchBar';
+
+function Header() {
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearch = (query) => {
+        setSearchQuery(query);
+
+    };
+
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+            <div className="flex items-center flex-shrink-0 text-white mr-6">
+                <NavLink to="/">
+                    <span className="font-semibold text-xl tracking-tight">Newsify</span>
+                </NavLink>
+            </div>
+            <SearchBar onSearch={handleSearch} />
+            <NavLink to="/saved-articles" className="ml-4 text-white hover:text-gray-400">
+                <FontAwesomeIcon icon={faBookmark} size="lg" />
+            </NavLink>
+        </nav>
+    );
+}
+
+export default Header;
